3c9ef7cfe921819ac91ee1fed051bfeb8d76df57
ClarisseFab <clarisse.fabreges@free.fr>
2024-04-24 17:50:12 +0200

    modif creation poules

--- script.js
+++ script.js
@@ -14,3 +14,48 @@ function ajoutEquipe() {
   }
 
 
+  function recupererEquipes(){
+  let listLi = document.getElementById("list-equipes").getElementsByTagName("li");
+  let arrayEquipes = [];
+  for(var j=0;j<listLi.length;j++){
+    arrayEquipes.push(listLi[j].innerHTML); 
+  }
+  return arrayEquipes;
+}
+
+
+function chunkArray(myArray, chunk_size){
+    var index = 0;
+    var arrayLength = myArray.length;
+    var tempArray = [];
+    
+    for (index = 0; index < arrayLength; index += chunk_size) {
+        myChunk = myArray.slice(index, index+chunk_size);
+        // Do something if you want with the group
+        tempArray.push(myChunk);
+    }
+
+    return tempArray;
+}
+
+function shuffleArray(array){
+    for (let i = array.length - 1; i > 0; i--) { 
+        let j = Math.floor(Math.random() * (i + 1)); 
+        [array[i], array[j]] = [array[j], array[i]]; 
+      } 
+      return array;
+}
+
+function creerPoules(){
+    let array = recupererEquipes();
+    if (array.length<9){
+        alert("ajoutez d'autres équipes"); 
+    } 
+    else{
+     let shuffleArray = shuffleArray(array);
+     let arrayCoupe = chunkArray(shuffleArray, shuffleArray.length/3);
+     console.log(arrayCoupe); 
+    }
+}
+
+
